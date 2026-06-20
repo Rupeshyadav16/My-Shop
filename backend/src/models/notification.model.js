e90b477ee86cf0b49020c0ff5e84b30dac719748
Rupesh Kumar <ry728309@gmail.com>
2026-06-20 10:06:05 +0530

    add missing notification model

---
+++ backend/src/models/notification.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const notificationSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    clerkId: {
+      type: String,
+    },
+    orderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Order",
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      default: "general",
+    },
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Notification = mongoose.model("Notification", notificationSchema);
